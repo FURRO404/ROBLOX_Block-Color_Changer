48276d9554420562a52c09f4cbf8a517383e3cce
NotSoToothless <67082114+FURRO404@users.noreply.github.com>
2021-06-14 17:36:31 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -62,9 +62,70 @@ function RandomColorChanger()
 	end
 end
 
+function RandomPositionChanger()
+	local block = BlockArray[math.random(1, 3)]
+	x = math.random(-5,5)
+	y = math.random(-3,3)
+	z = math.random(-5,5)
+	
+	if block == "Block_One" then
+		game.Workspace.Block_One.Position = game.Workspace.Block_One.Position + Vector3.new(x, y, z)
+		print("Moved Block One's position to", game.Workspace.Block_One.Position)
+
+	elseif block == "Block_Two" then
+		game.Workspace.Block_Two.Position = game.Workspace.Block_Two.Position + Vector3.new(x, y, z)
+		print("Moved Block Two's position to", game.Workspace.Block_Two.Position)
+
+	elseif block == "Block_Three" then
+		game.Workspace.Block_Three.Position = game.Workspace.Block_Three.Position + Vector3.new(x, y, z)
+		print("Moved Block Three's position to", game.Workspace.Block_Three.Position)
+	end
+	
+end
+
+-- CALCULATOR AREA --
+function ADD()
+	return print("The sum is", x+y)
+end
+
+function SUBTRACT()
+	return print("The difference is", math.floor(x-y + 0.5))
+end
+
+function DIVIDE()
+	return print("The quotient is", math.floor(x-y + 0.5))
+end
+
+function MULTIPLY()
+	return print("The product is", math.floor(x-y + 0.5))
+end
+
+function REMAINDER()
+	if x % y ~= 0 then
+		return print("The remainder is", math.floor(x-y + 0.5))
+			
+	elseif x % y == 0 then
+		 return print("No remainder")
+	end
+end
+
+function Calculator()
+	x = game.Workspace.Block_One.Size.x
+	y = game.Workspace.Block_One.Size.y
+	ADD(x, y)
+	SUBTRACT(x, y)
+	DIVIDE(x, y)
+	MULTIPLY(x, y)
+	REMAINDER(x, y)	
+end
+-- CALCULATOR AREA --
+
 while true do
 	RandomSizeChanger()
-	RandomColorChanger()
+ 	RandomColorChanger()
+	RandomPositionChanger()
+	--print("NEW LINE")
+	--Calculator()
 	wait(0.1)
 end
 -- End Script --
